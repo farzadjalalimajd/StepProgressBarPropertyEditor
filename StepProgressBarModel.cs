1bc451d5e0c2ee4a17b677685603c38b469d7352
FarzadJM <farzadjalalimajd@gmail.com>
2025-07-16 11:51:04 +0330

    Add project files.

---
+++ StepProgressBarModel.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.ExpressApp.Blazor.Components.Models;
+using JMTech.ExpressApp.Blazor.Editors.Renderers;
+
+namespace JMTech.ExpressApp.Blazor.Editors.Models;
+
+public class StepProgressBarModel : ComponentModelBase
+{
+    public int Value
+    {
+        get => GetPropertyValue<int>(0);
+        set => SetPropertyValue(value);
+    }
+
+    public List<string> Steps
+    {
+        get => GetPropertyValue<List<string>>([]);
+        set => SetPropertyValue(value);
+    }
+
+    public override Type ComponentType => typeof(StepProgressBarRenderer);
+}
---
+++ StepProgressBarPropertyEditor.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.ExpressApp.Blazor.Components.Models;
+using DevExpress.ExpressApp.Blazor.Editors;
+using DevExpress.ExpressApp.Model;
+using JMTech.ExpressApp.Blazor.Editors.Models;
+
+namespace JMTech.ExpressApp.Blazor.Editors;
+
+public class StepProgressBarPropertyEditor : BlazorPropertyEditorBase
+{
+    public StepProgressBarPropertyEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model)
+    {
+    }
+
+    public override StepProgressBarModel ComponentModel => (StepProgressBarModel)base.ComponentModel;
+
+    protected override IComponentModel CreateComponentModel() => new StepProgressBarModel();
+
+    protected override void ReadValueCore()
+    {
+        base.ReadValueCore();
+        ComponentModel.Value = (int)PropertyValue;
+    }
+}
